708da64bdb8e370fbaef7c45acb84164e633c891
Hashini Emalsha <hashiniemalsha06@gmail.com>
2026-08-14 11:27:07 +0530

    first commit

---
+++ aad_final_project/src/main/java/lk/ijse/aad_final_project/AadFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.aad_final_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AadFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AadFinalProjectApplication.class, args);
+	}
+
+}
---
+++ aad_final_project/src/test/java/lk/ijse/aad_final_project/AadFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.aad_final_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AadFinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
