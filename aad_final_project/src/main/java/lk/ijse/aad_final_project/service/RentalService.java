1fe7d43c76da6f85a9c8ad950154d7145ab0cede
Hashini Emalsha <hashiniemalsha06@gmail.com>
2026-08-22 10:32:42 +0530

    Implement Rental service and service implementation methods

--- aad_final_project/src/main/java/lk/ijse/aad_final_project/service/RentalService.java
+++ aad_final_project/src/main/java/lk/ijse/aad_final_project/service/RentalService.java
@@ -1,4 +1,18 @@
 package lk.ijse.aad_final_project.service;
 
+import lk.ijse.aad_final_project.dto.RentalDTO;
+
+import java.util.List;
+
 public interface RentalService {
+
+    void saveRental(RentalDTO rentalDTO);
+
+    List<RentalDTO> getAllRentals();
+
+    RentalDTO selectRental(Long rentalId);
+
+    void updateRental(RentalDTO rentalDTO);
+
+    void deleteRental(Long rentalId);
 }
--- aad_final_project/src/main/java/lk/ijse/aad_final_project/service/impl/RentalServiceImpl.java
+++ aad_final_project/src/main/java/lk/ijse/aad_final_project/service/impl/RentalServiceImpl.java
@@ -1,4 +1,185 @@
 package lk.ijse.aad_final_project.service.impl;
 
-public class RentalServiceImpl {
+import lk.ijse.aad_final_project.dto.RentalDTO;
+import lk.ijse.aad_final_project.entity.Customer;
+import lk.ijse.aad_final_project.entity.Rental;
+import lk.ijse.aad_final_project.entity.RentalRate;
+import lk.ijse.aad_final_project.entity.Vehicle;
+import lk.ijse.aad_final_project.repository.CustomerRepository;
+import lk.ijse.aad_final_project.repository.RentalRateRepository;
+import lk.ijse.aad_final_project.repository.RentalRepository;
+import lk.ijse.aad_final_project.repository.VehicleRepository;
+import lk.ijse.aad_final_project.service.RentalService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class RentalServiceImpl implements RentalService {
+
+    private final RentalRepository rentalRepository;
+    private final CustomerRepository customerRepository;
+    private final VehicleRepository vehicleRepository;
+    private final RentalRateRepository rentalRateRepository;
+
+    @Override
+    public void saveRental(RentalDTO rentalDTO) {
+        Optional<Customer> optionalCustomer = customerRepository.findById(rentalDTO.getCustomerId());
+
+        if (optionalCustomer.isEmpty()) {
+            throw new RuntimeException("Customer not found");
+        }
+
+        Optional<Vehicle> optionalVehicle = vehicleRepository.findById(rentalDTO.getVehicleId());
+
+        if (optionalVehicle.isEmpty()) {
+            throw new RuntimeException("Vehicle not found");
+        }
+
+        Optional<RentalRate> optionalRentalRate = rentalRateRepository.findById(rentalDTO.getRentalRateId());
+        if (optionalRentalRate.isEmpty()) {
+            throw new RuntimeException("Rental rate not found");
+        }
+
+        Customer customer = optionalCustomer.get();
+        Vehicle vehicle = optionalVehicle.get();
+        RentalRate rentalRate = optionalRentalRate.get();
+
+        Rental rental = new Rental();
+
+        rental.setStartDate(rentalDTO.getStartDate());
+        rental.setEndDate(rentalDTO.getEndDate());
+        rental.setRentalDays(rentalDTO.getRentalDays());
+        rental.setPickupMileage(rentalDTO.getPickupMileage());
+        rental.setReturnMileage(rentalDTO.getReturnMileage());
+        rental.setDepositAmount(rentalDTO.getDepositAmount());
+        rental.setStatus(rentalDTO.getStatus());
+        rental.setTotalAmount(rentalDTO.getTotalAmount());
+
+        rental.setCustomer(customer);
+        rental.setVehicle(vehicle);
+        rental.setRentalRate(rentalRate);
+
+        rentalRepository.save(rental);
+    }
+
+    @Override
+    public List<RentalDTO> getAllRentals() {
+        List<Rental> rentals = rentalRepository.findAll();
+
+        List<RentalDTO> rentalDTOList = new ArrayList<>();
+
+        for (Rental rental : rentals) {
+
+            RentalDTO rentalDTO = new RentalDTO();
+
+            rentalDTO.setRentalId(rental.getRentalId());
+            rentalDTO.setStartDate(rental.getStartDate());
+            rentalDTO.setEndDate(rental.getEndDate());
+            rentalDTO.setRentalDays(rental.getRentalDays());
+            rentalDTO.setPickupMileage(rental.getPickupMileage());
+            rentalDTO.setReturnMileage(rental.getReturnMileage());
+            rentalDTO.setDepositAmount(rental.getDepositAmount());
+            rentalDTO.setStatus(rental.getStatus());
+            rentalDTO.setTotalAmount(rental.getTotalAmount());
+            rentalDTO.setCustomerId(rental.getCustomer().getCustomerId());
+            rentalDTO.setVehicleId(rental.getVehicle().getVehicleId());
+            rentalDTO.setRentalRateId(rental.getRentalRate().getRateId());
+
+            rentalDTOList.add(rentalDTO);
+        }
+
+        return rentalDTOList;
+
+        }
+
+    @Override
+    public RentalDTO selectRental(Long rentalId) {
+        Optional<Rental> optionalRental = rentalRepository.findById(rentalId);
+
+        if (optionalRental.isEmpty()) {
+            throw new RuntimeException("Rental not found");
+        }
+
+        Rental rental = optionalRental.get();
+
+        RentalDTO rentalDTO = new RentalDTO();
+
+        rentalDTO.setRentalId(rental.getRentalId());
+        rentalDTO.setStartDate(rental.getStartDate());
+        rentalDTO.setEndDate(rental.getEndDate());
+        rentalDTO.setRentalDays(rental.getRentalDays());
+        rentalDTO.setPickupMileage(rental.getPickupMileage());
+        rentalDTO.setReturnMileage(rental.getReturnMileage());
+        rentalDTO.setDepositAmount(rental.getDepositAmount());
+        rentalDTO.setStatus(rental.getStatus());
+        rentalDTO.setTotalAmount(rental.getTotalAmount());
+        rentalDTO.setCustomerId(rental.getCustomer().getCustomerId());
+        rentalDTO.setVehicleId(rental.getVehicle().getVehicleId());
+        rentalDTO.setRentalRateId(rental.getRentalRate().getRateId());
+
+        return rentalDTO;
+
+    }
+
+    @Override
+    public void updateRental(RentalDTO rentalDTO) {
+        Optional<Rental> optionalRental = rentalRepository.findById(rentalDTO.getRentalId());
+
+        if (optionalRental.isEmpty()) {
+            throw new RuntimeException("Rental not found");
+        }
+
+        Optional<Customer> optionalCustomer = customerRepository.findById(rentalDTO.getCustomerId());
+
+        if (optionalCustomer.isEmpty()) {
+            throw new RuntimeException("Customer not found");
+        }
+
+        Optional<Vehicle> optionalVehicle = vehicleRepository.findById(rentalDTO.getVehicleId());
+
+        if (optionalVehicle.isEmpty()) {
+            throw new RuntimeException("Vehicle not found");
+        }
+        Optional<RentalRate> optionalRentalRate = rentalRateRepository.findById(rentalDTO.getRentalRateId());
+
+        if (optionalRentalRate.isEmpty()) {
+            throw new RuntimeException("Rental rate not found");
+        }
+
+        Rental rental = optionalRental.get();
+
+        Customer customer = optionalCustomer.get();
+        Vehicle vehicle = optionalVehicle.get();
+        RentalRate rentalRate = optionalRentalRate.get();
+
+        rental.setStartDate(rentalDTO.getStartDate());
+        rental.setEndDate(rentalDTO.getEndDate());
+        rental.setRentalDays(rentalDTO.getRentalDays());
+        rental.setPickupMileage(rentalDTO.getPickupMileage());
+        rental.setReturnMileage(rentalDTO.getReturnMileage());
+        rental.setDepositAmount(rentalDTO.getDepositAmount());
+        rental.setStatus(rentalDTO.getStatus());
+        rental.setTotalAmount(rentalDTO.getTotalAmount());
+
+        rental.setCustomer(customer);
+        rental.setVehicle(vehicle);
+        rental.setRentalRate(rentalRate);
+
+        rentalRepository.save(rental);
+
+    }
+
+    @Override
+    public void deleteRental(Long rentalId) {
+        if (!rentalRepository.existsById(rentalId)) {
+            throw new RuntimeException("Rental not found");
+        }
+
+        rentalRepository.deleteById(rentalId);
+    }
 }
