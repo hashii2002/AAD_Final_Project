a5b8bfe5ce85202e57632b14c49c9bec7c80d58b
Hashini Emalsha <hashiniemalsha06@gmail.com>
2026-08-22 09:34:10 +0530

    Implement vehicleModel service and service implementation methods

--- aad_final_project/src/main/java/lk/ijse/aad_final_project/service/VehicleModelService.java
+++ aad_final_project/src/main/java/lk/ijse/aad_final_project/service/VehicleModelService.java
@@ -1,4 +1,18 @@
 package lk.ijse.aad_final_project.service;
 
+import lk.ijse.aad_final_project.dto.VehicleModelDTO;
+
+import java.util.List;
+
 public interface VehicleModelService {
+
+    void saveVehicleModel(VehicleModelDTO vehicleModelDTO);
+
+    List<VehicleModelDTO> getAllVehicleModels();
+
+    VehicleModelDTO selectVehicleModel(Long modelId);
+
+    void updateVehicleModel(VehicleModelDTO vehicleModelDTO);
+
+    void deleteVehicleModel(Long modelId);
 }
--- aad_final_project/src/main/java/lk/ijse/aad_final_project/service/impl/VehicleModelServiceImpl.java
+++ aad_final_project/src/main/java/lk/ijse/aad_final_project/service/impl/VehicleModelServiceImpl.java
@@ -1,4 +1,127 @@
 package lk.ijse.aad_final_project.service.impl;
 
-public class VehicleModelServiceImpl {
+import lk.ijse.aad_final_project.dto.VehicleModelDTO;
+import lk.ijse.aad_final_project.entity.VehicleBrand;
+import lk.ijse.aad_final_project.entity.VehicleModel;
+import lk.ijse.aad_final_project.repository.VehicleBrandRepository;
+import lk.ijse.aad_final_project.repository.VehicleModelRepository;
+import lk.ijse.aad_final_project.service.VehicleModelService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class VehicleModelServiceImpl implements VehicleModelService {
+
+    private final VehicleModelRepository vehicleModelRepository;
+    private final VehicleBrandRepository vehicleBrandRepository;
+
+    @Override
+    public void saveVehicleModel(VehicleModelDTO vehicleModelDTO) {
+        Optional<VehicleBrand> optionalBrand = vehicleBrandRepository.findById(vehicleModelDTO.getBrandId());
+
+        if (optionalBrand.isEmpty()) {
+            throw new RuntimeException("Vehicle brand not found");
+        }
+
+        VehicleBrand vehicleBrand = optionalBrand.get();
+
+        VehicleModel vehicleModel = new VehicleModel();
+
+        vehicleModel.setModelName(vehicleModelDTO.getModelName());
+        vehicleModel.setFuelType(vehicleModelDTO.getFuelType());
+        vehicleModel.setSeatingCapacity(vehicleModelDTO.getSeatingCapacity());
+        vehicleModel.setTransmissionType(vehicleModelDTO.getTransmissionType());
+        vehicleModel.setBrand(vehicleBrand);
+
+        vehicleModelRepository.save(vehicleModel);
+
+    }
+
+    @Override
+    public List<VehicleModelDTO> getAllVehicleModels() {
+        List<VehicleModel> vehicleModels = vehicleModelRepository.findAll();
+
+        List<VehicleModelDTO> vehicleModelDTOList = new ArrayList<>();
+
+        for (VehicleModel vehicleModel : vehicleModels) {
+
+            VehicleModelDTO vehicleModelDTO = new VehicleModelDTO();
+
+            vehicleModelDTO.setModelId(vehicleModel.getModelId());
+            vehicleModelDTO.setBrandId(vehicleModel.getBrand().getBrandId());
+            vehicleModelDTO.setModelName(vehicleModel.getModelName());
+            vehicleModelDTO.setFuelType(vehicleModel.getFuelType());
+            vehicleModelDTO.setSeatingCapacity(vehicleModel.getSeatingCapacity());
+            vehicleModelDTO.setTransmissionType(vehicleModel.getTransmissionType());
+
+            vehicleModelDTOList.add(vehicleModelDTO);
+        }
+
+        return vehicleModelDTOList;
+    }
+
+    @Override
+    public VehicleModelDTO selectVehicleModel(Long modelId) {
+        Optional<VehicleModel> optionalVehicleModel = vehicleModelRepository.findById(modelId);
+
+        if (optionalVehicleModel.isEmpty()) {
+            throw new RuntimeException("Vehicle model not found");
+        }
+
+        VehicleModel vehicleModel = optionalVehicleModel.get();
+
+        VehicleModelDTO vehicleModelDTO = new VehicleModelDTO();
+
+        vehicleModelDTO.setModelId(vehicleModel.getModelId());
+        vehicleModelDTO.setBrandId(vehicleModel.getBrand().getBrandId());
+        vehicleModelDTO.setModelName(vehicleModel.getModelName());
+        vehicleModelDTO.setFuelType(vehicleModel.getFuelType());
+        vehicleModelDTO.setSeatingCapacity(vehicleModel.getSeatingCapacity());
+        vehicleModelDTO.setTransmissionType(vehicleModel.getTransmissionType());
+
+        return vehicleModelDTO;
+    }
+
+    @Override
+    public void updateVehicleModel(VehicleModelDTO vehicleModelDTO) {
+        Optional<VehicleModel> optionalVehicleModel = vehicleModelRepository.findById(vehicleModelDTO.getModelId());
+
+        if (optionalVehicleModel.isEmpty()) {
+            throw new RuntimeException("Vehicle model not found");
+        }
+
+        Optional<VehicleBrand> optionalBrand = vehicleBrandRepository.findById(vehicleModelDTO.getBrandId());
+
+        if (optionalBrand.isEmpty()) {
+            throw new RuntimeException("Vehicle brand not found");
+        }
+
+        VehicleModel vehicleModel = optionalVehicleModel.get();
+
+        VehicleBrand vehicleBrand = optionalBrand.get();
+
+        vehicleModel.setModelName(vehicleModelDTO.getModelName());
+        vehicleModel.setFuelType(vehicleModelDTO.getFuelType());
+        vehicleModel.setSeatingCapacity(vehicleModelDTO.getSeatingCapacity());
+        vehicleModel.setTransmissionType(vehicleModelDTO.getTransmissionType());
+        vehicleModel.setBrand(vehicleBrand);
+
+        vehicleModelRepository.save(vehicleModel);
+
+    }
+
+    @Override
+    public void deleteVehicleModel(Long modelId) {
+        if (!vehicleModelRepository.existsById(modelId)) {
+            throw new RuntimeException("Vehicle model not found");
+        }
+
+        vehicleModelRepository.deleteById(modelId);
+
+    }
 }
