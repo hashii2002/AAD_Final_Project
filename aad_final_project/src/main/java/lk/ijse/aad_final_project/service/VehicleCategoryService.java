e9ae564a33627b3a1e3479d79d1ac84a81b3226f
Hashini Emalsha <hashiniemalsha06@gmail.com>
2026-08-22 07:57:14 +0530

    Implement vehicleCategory service and service implementation methods

--- aad_final_project/src/main/java/lk/ijse/aad_final_project/service/VehicleCategoryService.java
+++ aad_final_project/src/main/java/lk/ijse/aad_final_project/service/VehicleCategoryService.java
@@ -1,4 +1,17 @@
 package lk.ijse.aad_final_project.service;
 
+import lk.ijse.aad_final_project.dto.VehicleCategoryDTO;
+
+import java.util.List;
+
 public interface VehicleCategoryService {
+    void saveVehicleCategory(VehicleCategoryDTO vehicleCategoryDTO);
+
+    List<VehicleCategoryDTO> getAllVehicleCategories();
+
+    VehicleCategoryDTO selectVehicleCategory(Long categoryId);
+
+    void updateVehicleCategory(VehicleCategoryDTO vehicleCategoryDTO);
+
+    void deleteVehicleCategory(Long categoryId);
 }
--- aad_final_project/src/main/java/lk/ijse/aad_final_project/service/impl/VehicleCategoryServiceImpl.java
+++ aad_final_project/src/main/java/lk/ijse/aad_final_project/service/impl/VehicleCategoryServiceImpl.java
@@ -1,4 +1,97 @@
 package lk.ijse.aad_final_project.service.impl;
 
-public class VehicleCategoryServiceImpl {
+import lk.ijse.aad_final_project.dto.VehicleCategoryDTO;
+import lk.ijse.aad_final_project.entity.VehicleCategory;
+import lk.ijse.aad_final_project.repository.VehicleCategoryRepository;
+import lk.ijse.aad_final_project.service.VehicleCategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class VehicleCategoryServiceImpl implements VehicleCategoryService {
+
+    private final VehicleCategoryRepository vehicleCategoryRepository;
+
+    @Override
+    public void saveVehicleCategory(VehicleCategoryDTO vehicleCategoryDTO) {
+        VehicleCategory vehicleCategory = new VehicleCategory();
+
+        vehicleCategory.setCategory(vehicleCategoryDTO.getCategory());
+        vehicleCategory.setDescription(vehicleCategoryDTO.getDescription());
+
+        vehicleCategoryRepository.save(vehicleCategory);
+
+    }
+
+    @Override
+    public List<VehicleCategoryDTO> getAllVehicleCategories() {
+        List<VehicleCategory> vehicleCategories = vehicleCategoryRepository.findAll();
+
+        List<VehicleCategoryDTO> vehicleCategoryDTOList = new ArrayList<>();
+
+        for (VehicleCategory vehicleCategory : vehicleCategories) {
+
+            VehicleCategoryDTO vehicleCategoryDTO = new VehicleCategoryDTO();
+
+            vehicleCategoryDTO.setCategoryId(vehicleCategory.getCategoryId());
+            vehicleCategoryDTO.setCategory(vehicleCategory.getCategory());
+            vehicleCategoryDTO.setDescription(vehicleCategory.getDescription());
+
+            vehicleCategoryDTOList.add(vehicleCategoryDTO);
+        }
+
+        return vehicleCategoryDTOList;
+    }
+
+    @Override
+    public VehicleCategoryDTO selectVehicleCategory(Long categoryId) {
+        Optional<VehicleCategory> optionalVehicleCategory = vehicleCategoryRepository.findById(categoryId);
+
+        if (optionalVehicleCategory.isEmpty()) {
+            throw new RuntimeException("Vehicle category not found");
+        }
+
+        VehicleCategory vehicleCategory = optionalVehicleCategory.get();
+
+        VehicleCategoryDTO vehicleCategoryDTO = new VehicleCategoryDTO();
+
+        vehicleCategoryDTO.setCategoryId(vehicleCategory.getCategoryId());
+        vehicleCategoryDTO.setCategory(vehicleCategory.getCategory());
+        vehicleCategoryDTO.setDescription(vehicleCategory.getDescription());
+
+        return vehicleCategoryDTO;
+    }
+
+    @Override
+    public void updateVehicleCategory(VehicleCategoryDTO vehicleCategoryDTO) {
+
+        Optional<VehicleCategory> optionalVehicleCategory = vehicleCategoryRepository.findById(vehicleCategoryDTO.getCategoryId());
+
+        if (optionalVehicleCategory.isEmpty()) {
+            throw new RuntimeException("Vehicle category not found");
+        }
+
+        VehicleCategory vehicleCategory = optionalVehicleCategory.get();
+
+        vehicleCategory.setCategory(vehicleCategoryDTO.getCategory());
+        vehicleCategory.setDescription(vehicleCategoryDTO.getDescription());
+
+        vehicleCategoryRepository.save(vehicleCategory);
+    }
+
+    @Override
+    public void deleteVehicleCategory(Long categoryId) {
+
+        if (!vehicleCategoryRepository.existsById(categoryId)) {
+            throw new RuntimeException("Vehicle category not found");
+        }
+
+        vehicleCategoryRepository.deleteById(categoryId);
+
+    }
 }
