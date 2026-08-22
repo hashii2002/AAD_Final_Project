9f7a654dc3b1e483ce670b01a4155ed7f44e1537
Hashini Emalsha <hashiniemalsha06@gmail.com>
2026-08-22 07:35:17 +0530

    Implement service user and service implementation methods

--- aad_final_project/src/main/java/lk/ijse/aad_final_project/service/UserService.java
+++ aad_final_project/src/main/java/lk/ijse/aad_final_project/service/UserService.java
@@ -1,4 +1,17 @@
 package lk.ijse.aad_final_project.service;
 
+import lk.ijse.aad_final_project.dto.UserDTO;
+
+import java.util.List;
+
 public interface UserService {
+    void saveUser(UserDTO userDTO);
+
+    List<UserDTO> getAllUsers();
+
+    UserDTO selectUser(Long userId);
+
+    void updateUser(UserDTO userDTO);
+
+    void deleteUser(Long userId);
 }
--- aad_final_project/src/main/java/lk/ijse/aad_final_project/service/impl/UserServiceImpl.java
+++ aad_final_project/src/main/java/lk/ijse/aad_final_project/service/impl/UserServiceImpl.java
@@ -1,4 +1,142 @@
 package lk.ijse.aad_final_project.service.impl;
 
-public class UserServiceImpl {
+import lk.ijse.aad_final_project.dto.UserDTO;
+import lk.ijse.aad_final_project.entity.Role;
+import lk.ijse.aad_final_project.entity.User;
+import lk.ijse.aad_final_project.repository.RoleRepository;
+import lk.ijse.aad_final_project.repository.UserRepository;
+import lk.ijse.aad_final_project.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+
+    @Override
+    public void saveUser(UserDTO userDTO) {
+        Optional<Role> optionalRole =
+                roleRepository.findById(userDTO.getRoleId());
+
+        if (optionalRole.isEmpty()) {
+            throw new RuntimeException("Role not found");
+        }
+
+        Role role = optionalRole.get();
+
+        User user = new User();
+
+        user.setUsername(userDTO.getUsername());
+        user.setEmail(userDTO.getEmail());
+        user.setPassword(userDTO.getPassword());
+        user.setFirstName(userDTO.getFirstName());
+        user.setLastName(userDTO.getLastName());
+        user.setPhone(userDTO.getPhone());
+        user.setStatus(userDTO.getStatus());
+        user.setRole(role);
+
+        userRepository.save(user);
+
+    }
+
+    @Override
+    public List<UserDTO> getAllUsers() {
+        List<User> users = userRepository.findAll();
+
+        List<UserDTO> userDTOList = new ArrayList<>();
+
+        for (User user : users) {
+
+            UserDTO userDTO = new UserDTO();
+
+            userDTO.setUserId(user.getUserId());
+            userDTO.setUsername(user.getUsername());
+            userDTO.setEmail(user.getEmail());
+            userDTO.setPassword(user.getPassword());
+            userDTO.setFirstName(user.getFirstName());
+            userDTO.setLastName(user.getLastName());
+            userDTO.setPhone(user.getPhone());
+            userDTO.setStatus(user.getStatus());
+            userDTO.setRoleId(user.getRole().getRoleId());
+
+            userDTOList.add(userDTO);
+        }
+
+        return userDTOList;
+    }
+
+    @Override
+    public UserDTO selectUser(Long userId) {
+        Optional<User> optionalUser =
+                userRepository.findById(userId);
+
+        if (optionalUser.isEmpty()) {
+            throw new RuntimeException("User not found");
+        }
+
+        User user = optionalUser.get();
+
+        UserDTO userDTO = new UserDTO();
+
+        userDTO.setUserId(user.getUserId());
+        userDTO.setUsername(user.getUsername());
+        userDTO.setEmail(user.getEmail());
+        userDTO.setPassword(user.getPassword());
+        userDTO.setFirstName(user.getFirstName());
+        userDTO.setLastName(user.getLastName());
+        userDTO.setPhone(user.getPhone());
+        userDTO.setStatus(user.getStatus());
+        userDTO.setRoleId(user.getRole().getRoleId());
+
+        return userDTO;
+    }
+
+    @Override
+    public void updateUser(UserDTO userDTO) {
+        Optional<User> optionalUser =
+                userRepository.findById(userDTO.getUserId());
+
+        if (optionalUser.isEmpty()) {
+            throw new RuntimeException("User not found");
+        }
+
+        Optional<Role> optionalRole =
+                roleRepository.findById(userDTO.getRoleId());
+
+        if (optionalRole.isEmpty()) {
+            throw new RuntimeException("Role not found");
+        }
+
+        User user = optionalUser.get();
+        Role role = optionalRole.get();
+
+        user.setUsername(userDTO.getUsername());
+        user.setEmail(userDTO.getEmail());
+        user.setPassword(userDTO.getPassword());
+        user.setFirstName(userDTO.getFirstName());
+        user.setLastName(userDTO.getLastName());
+        user.setPhone(userDTO.getPhone());
+        user.setStatus(userDTO.getStatus());
+        user.setRole(role);
+
+        userRepository.save(user);
+
+    }
+
+    @Override
+    public void deleteUser(Long userId) {
+        if (!userRepository.existsById(userId)) {
+            throw new RuntimeException("User not found");
+        }
+
+        userRepository.deleteById(userId);
+
+    }
 }
