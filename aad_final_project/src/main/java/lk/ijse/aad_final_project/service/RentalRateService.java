c29efdf14b1471f37c2c12c98885cbd85b8b75c7
Hashini Emalsha <hashiniemalsha06@gmail.com>
2026-08-22 10:13:30 +0530

    Implement RenatalRate service and service implementation methods

--- aad_final_project/src/main/java/lk/ijse/aad_final_project/service/RentalRateService.java
+++ aad_final_project/src/main/java/lk/ijse/aad_final_project/service/RentalRateService.java
@@ -1,4 +1,18 @@
 package lk.ijse.aad_final_project.service;
 
+import lk.ijse.aad_final_project.dto.RentalRateDTO;
+
+import java.util.List;
+
 public interface RentalRateService {
+
+    void saveRentalRate(RentalRateDTO rentalRateDTO);
+
+    List<RentalRateDTO> getAllRentalRates();
+
+    RentalRateDTO selectRentalRate(Long rateId);
+
+    void updateRentalRate(RentalRateDTO rentalRateDTO);
+
+    void deleteRentalRate(Long rateId);
 }
--- aad_final_project/src/main/java/lk/ijse/aad_final_project/service/impl/RentalRateServiceImpl.java
+++ aad_final_project/src/main/java/lk/ijse/aad_final_project/service/impl/RentalRateServiceImpl.java
@@ -1,4 +1,126 @@
 package lk.ijse.aad_final_project.service.impl;
 
-public class RentalRateServiceImpl {
+import lk.ijse.aad_final_project.dto.RentalRateDTO;
+import lk.ijse.aad_final_project.entity.RentalRate;
+import lk.ijse.aad_final_project.entity.VehicleCategory;
+import lk.ijse.aad_final_project.repository.RentalRateRepository;
+import lk.ijse.aad_final_project.repository.VehicleCategoryRepository;
+import lk.ijse.aad_final_project.service.RentalRateService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class RentalRateServiceImpl implements RentalRateService {
+
+    private final RentalRateRepository rentalRateRepository;
+    private final VehicleCategoryRepository vehicleCategoryRepository;
+
+    @Override
+    public void saveRentalRate(RentalRateDTO rentalRateDTO) {
+        Optional<VehicleCategory> optionalCategory = vehicleCategoryRepository.findById(rentalRateDTO.getCategoryId());
+
+        if (optionalCategory.isEmpty()) {
+            throw new RuntimeException("Vehicle category not found");
+        }
+
+        VehicleCategory vehicleCategory = optionalCategory.get();
+
+        RentalRate rentalRate = new RentalRate();
+
+        rentalRate.setDailyRate(rentalRateDTO.getDailyRate());
+        rentalRate.setMonthlyRate(rentalRateDTO.getMonthlyRate());
+        rentalRate.setFreeKmPerDay(rentalRateDTO.getFreeKmPerDay());
+        rentalRate.setExtraKmPrice(rentalRateDTO.getExtraKmPrice());
+        rentalRate.setCategory(vehicleCategory);
+
+        rentalRateRepository.save(rentalRate);
+    }
+
+    @Override
+    public List<RentalRateDTO> getAllRentalRates() {
+        List<RentalRate> rentalRates = rentalRateRepository.findAll();
+
+        List<RentalRateDTO> rentalRateDTOList = new ArrayList<>();
+
+        for (RentalRate rentalRate : rentalRates) {
+
+            RentalRateDTO rentalRateDTO = new RentalRateDTO();
+
+            rentalRateDTO.setRateId(rentalRate.getRateId());
+            rentalRateDTO.setDailyRate(rentalRate.getDailyRate());
+            rentalRateDTO.setMonthlyRate(rentalRate.getMonthlyRate());
+            rentalRateDTO.setFreeKmPerDay(rentalRate.getFreeKmPerDay());
+            rentalRateDTO.setExtraKmPrice(rentalRate.getExtraKmPrice());
+            rentalRateDTO.setCategoryId(rentalRate.getCategory().getCategoryId());
+
+            rentalRateDTOList.add(rentalRateDTO);
+        }
+
+        return rentalRateDTOList;
+    }
+
+    @Override
+    public RentalRateDTO selectRentalRate(Long rateId) {
+        Optional<RentalRate> optionalRentalRate = rentalRateRepository.findById(rateId);
+
+        if (optionalRentalRate.isEmpty()) {
+            throw new RuntimeException("Rental rate not found");
+        }
+
+        RentalRate rentalRate = optionalRentalRate.get();
+
+        RentalRateDTO rentalRateDTO = new RentalRateDTO();
+
+        rentalRateDTO.setRateId(rentalRate.getRateId());
+        rentalRateDTO.setDailyRate(rentalRate.getDailyRate());
+        rentalRateDTO.setMonthlyRate(rentalRate.getMonthlyRate());
+        rentalRateDTO.setFreeKmPerDay(rentalRate.getFreeKmPerDay());
+        rentalRateDTO.setExtraKmPrice(rentalRate.getExtraKmPrice());
+        rentalRateDTO.setCategoryId(rentalRate.getCategory().getCategoryId());
+
+        return rentalRateDTO;
+    }
+
+    @Override
+    public void updateRentalRate(RentalRateDTO rentalRateDTO) {
+        Optional<RentalRate> optionalRentalRate = rentalRateRepository.findById(rentalRateDTO.getRateId());
+
+        if (optionalRentalRate.isEmpty()) {
+            throw new RuntimeException("Rental rate not found");
+        }
+
+        Optional<VehicleCategory> optionalCategory = vehicleCategoryRepository.findById(rentalRateDTO.getCategoryId());
+
+        if (optionalCategory.isEmpty()) {
+            throw new RuntimeException("Vehicle category not found");
+        }
+
+        RentalRate rentalRate = optionalRentalRate.get();
+
+        VehicleCategory vehicleCategory = optionalCategory.get();
+        rentalRate.setDailyRate(rentalRateDTO.getDailyRate());
+        rentalRate.setMonthlyRate(rentalRateDTO.getMonthlyRate());
+        rentalRate.setFreeKmPerDay(rentalRateDTO.getFreeKmPerDay());
+        rentalRate.setExtraKmPrice(rentalRateDTO.getExtraKmPrice());
+        rentalRate.setCategory(vehicleCategory);
+
+        rentalRateRepository.save(rentalRate);
+
+    }
+
+    @Override
+    public void deleteRentalRate(Long rateId) {
+
+        if (!rentalRateRepository.existsById(rateId)) {
+            throw new RuntimeException("Rental rate not found");
+        }
+
+        rentalRateRepository.deleteById(rateId);
+
+    }
 }
