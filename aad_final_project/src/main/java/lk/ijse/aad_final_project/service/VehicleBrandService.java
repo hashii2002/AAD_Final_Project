1dc2cfb792cfd6dff8cd8a625a9f489a20c710db
Hashini Emalsha <hashiniemalsha06@gmail.com>
2026-08-22 09:22:57 +0530

    Implement vehicleBrand service and service implementation methods

--- aad_final_project/src/main/java/lk/ijse/aad_final_project/service/VehicleBrandService.java
+++ aad_final_project/src/main/java/lk/ijse/aad_final_project/service/VehicleBrandService.java
@@ -1,4 +1,17 @@
 package lk.ijse.aad_final_project.service;
 
+import lk.ijse.aad_final_project.dto.VehicleBrandDTO;
+
+import java.util.List;
+
 public interface VehicleBrandService {
+    void saveVehicleBrand(VehicleBrandDTO vehicleBrandDTO);
+
+    List<VehicleBrandDTO> getAllVehicleBrands();
+
+    VehicleBrandDTO selectVehicleBrand(Long brandId);
+
+    void updateVehicleBrand(VehicleBrandDTO vehicleBrandDTO);
+
+    void deleteVehicleBrand(Long brandId);
 }
--- aad_final_project/src/main/java/lk/ijse/aad_final_project/service/impl/VehicleBrandServiceImpl.java
+++ aad_final_project/src/main/java/lk/ijse/aad_final_project/service/impl/VehicleBrandServiceImpl.java
@@ -1,4 +1,96 @@
 package lk.ijse.aad_final_project.service.impl;
 
-public class VehicleBrandServiceImpl {
+import lk.ijse.aad_final_project.dto.VehicleBrandDTO;
+import lk.ijse.aad_final_project.entity.VehicleBrand;
+import lk.ijse.aad_final_project.repository.VehicleBrandRepository;
+import lk.ijse.aad_final_project.service.VehicleBrandService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class VehicleBrandServiceImpl implements VehicleBrandService {
+
+    private final VehicleBrandRepository vehicleBrandRepository;
+
+    @Override
+    public void saveVehicleBrand(VehicleBrandDTO vehicleBrandDTO) {
+        VehicleBrand vehicleBrand = new VehicleBrand();
+
+        vehicleBrand.setBrandName(vehicleBrandDTO.getBrandName());
+        vehicleBrand.setCountry(vehicleBrandDTO.getCountry());
+
+        vehicleBrandRepository.save(vehicleBrand);
+
+    }
+
+    @Override
+    public List<VehicleBrandDTO> getAllVehicleBrands() {
+        List<VehicleBrand> vehicleBrands = vehicleBrandRepository.findAll();
+
+        List<VehicleBrandDTO> vehicleBrandDTOList = new ArrayList<>();
+
+        for (VehicleBrand vehicleBrand : vehicleBrands) {
+
+            VehicleBrandDTO vehicleBrandDTO = new VehicleBrandDTO();
+
+            vehicleBrandDTO.setBrandId(vehicleBrand.getBrandId());
+            vehicleBrandDTO.setBrandName(vehicleBrand.getBrandName());
+            vehicleBrandDTO.setCountry(vehicleBrand.getCountry());
+
+            vehicleBrandDTOList.add(vehicleBrandDTO);
+        }
+
+        return vehicleBrandDTOList;
+    }
+
+    @Override
+    public VehicleBrandDTO selectVehicleBrand(Long brandId) {
+        Optional<VehicleBrand> optionalVehicleBrand = vehicleBrandRepository.findById(brandId);
+
+        if (optionalVehicleBrand.isEmpty()) {
+            throw new RuntimeException("Vehicle brand not found");
+        }
+
+        VehicleBrand vehicleBrand = optionalVehicleBrand.get();
+
+        VehicleBrandDTO vehicleBrandDTO = new VehicleBrandDTO();
+
+        vehicleBrandDTO.setBrandId(vehicleBrand.getBrandId());
+        vehicleBrandDTO.setBrandName(vehicleBrand.getBrandName());
+        vehicleBrandDTO.setCountry(vehicleBrand.getCountry());
+
+        return vehicleBrandDTO;
+    }
+
+    @Override
+    public void updateVehicleBrand(VehicleBrandDTO vehicleBrandDTO) {
+        Optional<VehicleBrand> optionalVehicleBrand = vehicleBrandRepository.findById(vehicleBrandDTO.getBrandId());
+
+        if (optionalVehicleBrand.isEmpty()) {
+            throw new RuntimeException("Vehicle brand not found");
+        }
+
+        VehicleBrand vehicleBrand = optionalVehicleBrand.get();
+
+        vehicleBrand.setBrandName(vehicleBrandDTO.getBrandName());
+        vehicleBrand.setCountry(vehicleBrandDTO.getCountry());
+
+        vehicleBrandRepository.save(vehicleBrand);
+
+    }
+
+    @Override
+    public void deleteVehicleBrand(Long brandId) {
+        if (!vehicleBrandRepository.existsById(brandId)) {
+            throw new RuntimeException("Vehicle brand not found");
+        }
+
+        vehicleBrandRepository.deleteById(brandId);
+
+    }
 }
